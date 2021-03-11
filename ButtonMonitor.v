e8e5584929afc59f78b74cc59d14bcc0764eff39
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-03-11 20:05:36 +0000

    buttonMonitor - reduced to one button
    - reduced module to watch a single button
    - defined ouputs and transitions for each state

--- ButtonMonitor.v
+++ ButtonMonitor.v
@@ -6,33 +6,57 @@
 //
 //Description
 //------------
-//Reads the value of each button and outputs 1 for 1 clock cycle when a button is pressed.
+//Detects the rising edge of the button press and outputs 1 clock cycle, .
 
+// declare module
 module buttonMonitor (
 	input clock,
 	input reset,
 	
-	output reg [3:0] buttonPresses
+	input buttonPress,
+	
+	output reg buttonEdge
 );
 
 // declare state-machine register
-reg [2:0] state;
+reg state;
 
 // define state names
-localparam IDLE_STATE = 3'd0;
-localparam BUTTON0_STATE = 3'd0;
-localparam BUTTON1_STATE = 3'd0;
-localparam BUTTON2_STATE = 3'd0;
-localparam BUTTON3_STATE = 3'd0;
+localparam LOW_STATE = 1'b0;
+localparam HIGH_STATE = 1'b1;
 
 // define state machine outputs and transitions
 always @ (posedge clock or posedge reset) begin
 	if (reset) begin
-		state <= IDLE_STATE;
-		buttonPresses <= 4'b0;
+		buttonEdge <= 1'b0;
+		state <= LOW_STATE;
 	end else begin
-	
-	
+		case (state)
+			LOW_STATE: begin
+				// wait in low state until button pressed
+				if (buttonPress) begin
+					// when button pressed move to high state and 
+					// set buttonedge high for 1 clock cycle
+					buttonEdge <= 1'b1;
+					state = HIGH_STATE;
+				end else begin 
+					buttonEdge <= 1'b0;
+					state = LOW_STATE;
+				end
+			end
+			
+			HIGH_STATE: begin
+				// wait in high state until button released
+				if (!buttonPress) begin
+					// if button released return to low state
+					buttonEdge <= 1'b0;
+					state = LOW_STATE;
+				end else begin 
+					buttonEdge <= 1'b0;
+					state = HIGH_STATE;
+				end
+			end
+		endcase
 	end
 end
 
