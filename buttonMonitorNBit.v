018cf1873439f7b24fef7829ba5a688bb6e76ad1
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-03-11 21:46:30 +0000

    buttonMonitorNBit - Instantiation of submodules
    - added generate function to generate buttonMonitor submodules

--- buttonMonitorNBit.v
+++ buttonMonitorNBit.v
@@ -18,10 +18,26 @@ module buttonMonitorNBit #(
 	
 	input [WIDTH-1:0] buttonPress,
 	
-	output reg [WIDTH-1:0] buttonEdge
+	output [WIDTH-1:0] buttonEdge
 );
 
+// declare loop variable
+genvar i;
 
-
+// instantiate submodules
+generate
+	for(i = 0; i <= WIDTH - 1; i = i + 1) begin : button_loop
+		// instantiate WIDTH buttonMonitors
+		buttonMonitor button (
+			.clock			(clock	),
+			.reset			(reset	),
+	
+			.buttonPress	(buttonPress[i]),
+	
+			.buttonEdge		(buttonEdge[i]	)
+		);
+	
+	end
+endgenerate
 
 endmodule 
