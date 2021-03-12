42a1f6f3c5ddba15ae37a22b75ab48eed6d4537e
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-03-12 17:03:16 +0000

    pinCodeTester - Defined reset condtions
    - defined the reset conditions
    - modified local params
    - added zero variable

--- pinCodeTester.v
+++ pinCodeTester.v
@@ -10,20 +10,24 @@
 
 module pinCodeTester #(
 	// declare parameters
-	parameter CODE_LENGTH = 4 // number of input buttons
+	parameter DIGITS = 4, // number of digits in unlock code
+	parameter CODE_LENGTH = 4*DIGITS // bits required to store unlock code 
 )(
 	// declare ports
 	input clock,
 	input reset,
 	
 	input key,
-	input [CODE_LENGTH-1:0] code,
+	input [CODE_LENGTH-1:0] pinCode,
 	
-	output unlock
+	output reg unlock
 );
 
 // declare pin storage register
 reg [CODE_LENGTH-1:0] pinEntry;
+
+// declare local variable
+localparam ZERO = {CODE_LENGTH{1'b0}};
 
 // declare state-machine register
 reg state;
@@ -33,8 +37,15 @@ localparam READ_STATE = 3'd0;
 localparam CHECK_STATE = 3'd1;
 localparam UNLOCK_STATE = 3'd3;
 
+// define state machine outputs and transitions
 always @ (posedge clock or posedge reset) begin
+	if (reset) begin
+		unlock <= 1'b0;
+		pinEntry <= 
+		state <= READ_STATE;
+	end else begin
 	
+	end
 	
 end
 
