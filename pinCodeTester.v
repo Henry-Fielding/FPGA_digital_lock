43536d74a0aa71bbea0d89e25b69e7f5e6cb99d4
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-03-13 10:02:32 +0000

    pinCodeTester- defined state behaviour and transitions
    - defined the state transitions and output behaviour for each state

--- pinCodeTester.v
+++ pinCodeTester.v
@@ -11,20 +11,23 @@
 module pinCodeTester #(
 	// declare parameters
 	parameter DIGITS = 4, // number of digits in unlock code
-	parameter CODE_LENGTH = 4*DIGITS // bits required to store unlock code 
+	parameter CODE_LENGTH = 4*DIGITS, // bits required to store unlock code
+	parameter COUNTER_WIDTH =  $clog2(DIGITS)
 )(
 	// declare ports
 	input clock,
 	input reset,
 	
-	input key,
+	input [3:0] key,
 	input [CODE_LENGTH-1:0] pinCode,
 	
-	output reg unlock
+	output reg unlock,
+	output reg [CODE_LENGTH-1:0] pinEntry
 );
 
 // declare pin storage register
-reg [CODE_LENGTH-1:0] pinEntry;
+//reg [CODE_LENGTH-1:0] pinEntry;
+reg [COUNTER_WIDTH-1:0] digitCounter;
 
 // declare local variable
 localparam ZERO = {CODE_LENGTH{1'b0}};
@@ -34,19 +37,53 @@ reg state;
 
 // define state names
 localparam READ_STATE = 3'd0;
-localparam CHECK_STATE = 3'd1;
+localparam COMPARE_STATE = 3'd1;
 localparam UNLOCK_STATE = 3'd3;
+localparam CLEAR_STATE = 3'd4;
 
 // define state machine outputs and transitions
 always @ (posedge clock or posedge reset) begin
 	if (reset) begin
 		unlock <= 1'b0;
-		pinEntry <= 
+		pinEntry <= ZERO;
 		state <= READ_STATE;
 	end else begin
-	
+		case (state)
+			READ_STATE : begin
+				unlock <= 1'b0;
+				
+				if (digitCounter ==  DIGITS) begin
+					state <= COMPARE_STATE;
+					
+				end else if (key == 4'b0001) begin
+					pinEntry = {pinEntry << 4, key};
+					digitCounter <= digitCounter + 1;
+					state <= READ_STATE;
+					
+				end else begin
+					state <= READ_STATE;
+				end
+			end
+			
+			COMPARE_STATE : begin
+				state <= CLEAR_STATE;
+			end
+		
+			UNLOCK_STATE : begin
+				unlock <= 1;
+				state <= CLEAR_STATE;
+			end
+			
+			CLEAR_STATE : begin
+				unlock <= 0;
+				pinEntry <= ZERO;
+				state <= READ_STATE;
+			end
+			
+			default state <= CLEAR_STATE;
+		
+		endcase
 	end
-	
 end
 
 endmodule
